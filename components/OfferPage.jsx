d32326d00e0f0bd4d74f1772c6b4c6d12ee6ada2
nasiruddin-an4 <nasir200087@diit.edu.bd>
2026-02-08 16:33:12 +0600

    feat: Increase grid column count for offer and skeleton cards on medium and large screens.

--- components/OfferPage.jsx
+++ components/OfferPage.jsx
@@ -51,7 +51,7 @@ const OfferPage = () => {
           </div>
 
           {/* Skeleton Grid */}
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+          <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-8">
             {[1, 2, 3, 4, 5, 6].map((i) => (
               <div
                 key={i}
@@ -112,7 +112,7 @@ const OfferPage = () => {
         </div>
 
         {/* Grid Section */}
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+        <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-8">
           {offers.map((offer) => (
             <Link
               href={`/promotions?category=${encodeURIComponent(offer.title)}`}
